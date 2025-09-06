ec5686bcb2dc46d8766c89b51e2424c98e406737
Zaid Siddiqui <szaidtech@gmail.com>
2025-09-06 16:38:33 +0530

    work done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,18 @@ import { Routes } from '@angular/router';
 import { AuthenticationRoutes } from './authentication/authentication.routes';
 import { DashboardRoutes } from './admin/dashboard.routes';
 import { AuthGuard } from '../app/guards/auth.guard'; // ✅ import guard
+import { LoginComponent } from './authentication/login/login';
+import { ForgotPasswordComponent } from './authentication/forgot-password/forgot-password';
+import { RegisterUserComponent } from './authentication/register-user/register-user';
 
 export const routes: Routes = [
-    {
-        path: '',
-        children: AuthenticationRoutes   // your existing auth routes
-    },
-    {
-        path: 'dashboard',
-        canActivate: [AuthGuard],        // ✅ protect dashboard
-        children: DashboardRoutes
-    }
+ 
+  { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'register-user', component: RegisterUserComponent },
+  {
+    path: 'dashboard',
+    // canActivate: [AuthGuard], // ✅ protect dashboard
+    children: DashboardRoutes,
+  },
 ];
